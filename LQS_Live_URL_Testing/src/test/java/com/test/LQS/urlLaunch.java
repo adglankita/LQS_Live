dc1e3812579b075378f5b4e64bc6a0d7cfa402ae
AGL <AGL@AGLGGNLAP0234>
2019-01-17 12:59:40 +0530

    Committing changes V3.0

--- LQS_Live_URL_Testing/src/test/java/com/test/LQS/urlLaunch.java
+++ LQS_Live_URL_Testing/src/test/java/com/test/LQS/urlLaunch.java
@@ -1,14 +1,17 @@
-package URL_Launching;
+package com.test.LQS;
 
 import java.util.concurrent.TimeUnit;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
 
-public class launchUrl {
-
-	public static void main(String[] args) {
-
+public class urlLaunch {
+	
+	
+	@Test
+	public void test_url() {
+		
 		WebDriver driver;
 		System.setProperty("webdriver.chrome.driver", "C:\\Users\\AGL\\Desktop\\Setup\\Important_downloads\\chromedriver_win32\\chromedriver.exe");
 		driver = new ChromeDriver();
@@ -20,9 +23,9 @@ public class launchUrl {
 		driver.manage().timeouts().implicitlyWait(90, TimeUnit.SECONDS);
 		
 		driver.close();
-		
-		
-
 	}
+	
+	
+	
 
 }
